e5f68a266cfa522587d40cdedc27d6934c7346f0
guizo <guizoxxv@gmail.com>
2019-03-25 21:15:31 -0300

    Faker DB

--- server/api/routes/guests.js
+++ server/api/routes/guests.js
@@ -1,7 +1,25 @@
+var faker = require('faker');
+var latinize = require('latinize');
+
 const express = require('express');
 const router = express.Router();
 const GuestsController = require('../controllers/guests');
 
+router.get('/faker', (req, res, next) => {
+    let guests = [];
+
+    for(let i = 0; i < 50; i++) {
+        let name = faker.name.findName();
+
+        guests.push({
+            name: name,
+            formatted_name: latinize(name).toLocaleLowerCase(),
+            present: faker.random.boolean(),
+        });
+    }
+
+    res.status(200).json(guests);
+});
 router.get('/', GuestsController.getGuests);
 router.put('/', GuestsController.updateGuestPresent);
 
