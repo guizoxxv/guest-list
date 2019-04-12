e80a5c410a60282df9bd29813c5642fadf7debd6
guizo <guizoxxv@gmail.com>
2019-04-12 00:49:47 -0300

    Restructure events.guests column

--- TODO.md
+++ TODO.md
@@ -4,27 +4,3 @@
 * Websocket
 * Upload de planilha
 * Efeitos
-* Armazenar guests como objetos com propriedadades uuid. Ex:
-
-{
-  "guests": {
-    "c035a449-2edb-4516-87ba-a773fbb7c9cd": {
-      "id": "c035a449-2edb-4516-87ba-a773fbb7c9cd",
-      "name": "Huguinho",
-      "present": true,
-      "formatted_name": "huguinho"
-    },
-    "fceb2d97-7795-4f0c-8c6a-f9e1fcb0cd35": {
-      "id": "fceb2d97-7795-4f0c-8c6a-f9e1fcb0cd35",
-      "name": "Zézinho",
-      "present": false,
-      "formatted_name": "zezinho"
-    },
-    "eed8e2d7-a31d-4f77-a208-9d2af47981ba": {
-      "id": "eed8e2d7-a31d-4f77-a208-9d2af47981ba",
-      "name": "Luizinho",
-      "present": true,
-      "formatted_name": "luizinho"
-    }
-  }
-}
--- server/api/controllers/guests.js
+++ server/api/controllers/guests.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 exports.getGuests = (req, res, next) => {
     pool.query('SELECT guests FROM events WHERE id = $1', [1])
         .then(result => {
-            res.status(200).json(result.rows[0] ? result.rows[0].guests : []);
+            res.status(200).json(result.rows[0] ? Object.values(result.rows[0].guests) : []);
         })
         .catch(e => console.error(e.stack))
 }
@@ -18,13 +18,13 @@ exports.getGuests = (req, res, next) => {
 exports.updateGuestPresent = (req, res, next) => {
     let guestId = req.body.id;
     let present = !req.body.present;
-    let path = `{guests, ${guestId}, present}`;
+    let path = `{${guestId}, present}`;
 
     pool.query(
             `UPDATE events
             SET guests = jsonb_set(guests, $1, $2::jsonb, false)
             WHERE id = $3
-            RETURNING guests->'guests'->$4 as guest`,
+            RETURNING guests->$4 as guest`,
             [path, present, 1, guestId]
         )
         .then(result => {
